80e97ec51743d69b3abd1500a1dfaa76f9c804c8
Kadiyala Siva Prasad <sivaprasad.ksp01@gmail.com>
2023-12-25 21:08:59 +0530

    Added the sidebar social icons

--- src/Portfolio/ProfilePage.js
+++ src/Portfolio/ProfilePage.js
@@ -16,8 +16,25 @@ export const ProfilePage = () => {
         </div>
       </div>
       <div className='side_bar_social_icons'>
-        <div id='side_bar_social_icons_line'>
-
+        <div id='inner_side_bar_social_icons'>
+          <div id='social_icons_01'>
+            <ul id='social_icons'>
+              <a  target='_blank' className=''>
+                <i class='bx bxl-linkedin'></i>
+              </a>
+              <a  target='_blank' className=''>
+                <i class='bx bxl-twitter' ></i>
+              </a>
+              <a  target='_blank' className=''>
+                <i className='bx bxl-instagram'></i>
+              </a>
+              <a target='_blank'>
+               <i class='bx bxl-github'></i>
+              </a>
+            </ul>
+          </div>
+          <div id='vertical_line_social_icons'></div>
+          
         </div>
       </div>
     </div>
