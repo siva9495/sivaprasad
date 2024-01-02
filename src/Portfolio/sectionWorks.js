0e332566c9b03cf9b7a4291fdd19b74179fc4d06
Kadiyala Siva Prasad <sivaprasad.ksp01@gmail.com>
2024-01-02 15:36:13 +0530

    Just add the under construction word in section works

--- src/Portfolio/sectionWorks.js
+++ src/Portfolio/sectionWorks.js
@@ -3,7 +3,9 @@ import './sectionWorks.css'
 
 export const sectionWorks = () => {
   return (
-    <div className='sectionWorks'>sectionWorks</div>
+    <div className='sectionWorks'>
+      <span id='under_construction'>Under Construction :)</span>
+    </div>
   )
 }
 export default sectionWorks;
