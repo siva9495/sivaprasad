4128b13e6f35f4b5b0b4dc4c450ac8e9775f9fc7
siva9495 <sivaprasad.ksp01@gmail.com>
2024-12-05 15:43:12 +0530

    added the section contact me

---
+++ src/SectionContactMe/SectionContactMe.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './SectionContactMe.css'
+
+const SectionContactMe = () => {
+  return (
+    <div>SectionContactMe</div>
+  )
+}
+
+export default SectionContactMe;
