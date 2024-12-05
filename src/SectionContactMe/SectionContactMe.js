7353027865aa2afdd473b8a3549239f0127402ec
siva9495 <sivaprasad.ksp01@gmail.com>
2024-12-05 18:36:01 +0530

    completed the contact me text html

--- src/SectionContactMe/SectionContactMe.js
+++ src/SectionContactMe/SectionContactMe.js
@@ -3,7 +3,17 @@ import './SectionContactMe.css'
 
 const SectionContactMe = () => {
   return (
-    <div>SectionContactMe</div>
+    <div className='section_contactme_container'>
+        <div className='section_contactme_content'>
+            <div className='scm_contact_text'>
+                <span className='scm_contact_text_span'>CONTACT</span>
+            </div>
+            <div className='scm_contactme_body'></div>
+            <div className='scm_me_text'>
+                <span className='scm_me_text_span'>ME</span>
+            </div>
+        </div>
+    </div>
   )
 }
 
